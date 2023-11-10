1e763228c658c0503a3789594e3d45150394f6bc
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-11-10 16:19:20 +0200

    lightbox added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const apiKey = '40581736-d4df81fdfae18c25a1eb903af';
 const baseUrl = 'https://pixabay.com/api/';
@@ -7,66 +9,33 @@ let pageNum = 1;
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
+  galleryList: document.querySelector('.gallery-list'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
 refs.loadMoreBtn.hidden = true;
 refs.searchForm.addEventListener('submit', onSearch);
-
+const lightbox = new SimpleLightbox('.gallery_link', {
+  captionsData: 'alt',
+  captionDelay: 200,
+});
 async function onSearch(e) {
   e.preventDefault();
   pageNum = 1;
 
-  // const searchParams = new URLSearchParams({
-  //   key: apiKey,
-  //   q: e.target.elements.searchQuery.value,
-  //   image_type: 'photo',
-  //   orientation: 'horizontal',
-  //   safesearch: 'true',
-  //   per_page: 40,
-  //   page: pageNum,
-  // });
+  refs.galleryList.innerHTML = '';
 
-  refs.gallery.innerHTML = '';
-  // // console.log(e.target.elements.searchQuery.value);
-  // const response = await axios.get(`${baseUrl}?${searchParams}`);
-  // const responseDataArr = response.data.hits;
-  // // console.log(response.data.hits);
   const photos = await fetchPhotos();
   renderPhotos(photos);
+  lightbox.refresh();
   pageNum += 1;
 
-  // if (response.data.totalHits > 40) {
-  //   refs.loadMoreBtn.hidden = false;
-  //   refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
-  // } else {
-  //   refs.loadMoreBtn.hidden = true;
-  //   alert("We're sorry, but you've reached the end of search results.");
-  // }
-
   async function onLoadMoreBtnClick() {
-    //   pageNum += 1;
-    //   const response = await axios.get(`${baseUrl}?${searchParams}`);
-    //   const responseDataArr = response.data.hits;
-
-    //   renderPhotos(responseDataArr);
-
-    //   if (response.data.totalHits > 40) {
-    //     refs.loadMoreBtn.hidden = false;
-    //     refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
-    //   } else {
-    //     refs.loadMoreBtn.hidden = true;
-    //     alert("We're sorry, but you've reached the end of search results.");
-    //   }
-    // }
     const photos = await fetchPhotos();
     renderPhotos(photos);
+    lightbox.refresh();
     pageNum += 1;
   }
   async function fetchPhotos() {
-    // const params = new URLSearchParams({
-    //   _limit: perPage,
-    //   _page: page
-    // });
     const searchParams = new URLSearchParams({
       key: apiKey,
       q: e.target.elements.searchQuery.value,
@@ -77,13 +46,9 @@ async function onSearch(e) {
       page: pageNum,
     });
 
-    // const response = await axios.get(
-    //   `https://jsonplaceholder.typicode.com/posts?${params}`
-    // );
-    // console.log(e.target.elements.searchQuery.value);
     const response = await axios.get(`${baseUrl}?${searchParams}`);
     const responseDataArr = response.data.hits;
-    // // console.log(response.data.hits);
+
     if (response.data.totalHits > 40) {
       refs.loadMoreBtn.hidden = false;
       refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
@@ -92,7 +57,11 @@ async function onSearch(e) {
       alert("We're sorry, but you've reached the end of search results.");
     }
     return responseDataArr;
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
+  // function renderPhotos(arr) {
   function photoTemplate(photo) {
     const {
       webformatURL,
@@ -102,11 +71,10 @@ async function onSearch(e) {
       views,
       comments,
       downloads,
-      id,
     } = photo;
 
-    return `<li class='list-item id="${id}"><div class="photo-card">
-	<img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    return `<li class="list-item" ><div class="photo-card">
+    <a href="${largeImageURL}" class="gallery_link"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
 	<div class="info">
 		<p class="info-item">
 			<b>${likes}</b>
@@ -123,14 +91,13 @@ async function onSearch(e) {
 	</div>
 </div></li>`;
   }
+
   function galleryTemplate(photosArray) {
     return photosArray.map(photoTemplate).join('\n');
   }
+
   function renderPhotos(photosArray) {
     const markup = galleryTemplate(photosArray);
-    refs.gallery.insertAdjacentHTML(
-      'beforeend',
-      `<ul class ='gallery-list'>${markup}</ul>`
-    );
+    refs.galleryList.insertAdjacentHTML('beforeend', markup);
   }
 }
