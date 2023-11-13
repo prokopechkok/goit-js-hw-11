fefd81a96199f1d5bec2207e1a94bb402470664e
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-11-13 23:02:26 +0200

    v1.1

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -4,9 +4,12 @@ import { refs } from './refs';
 
 export async function onSearch(e) {
   e.preventDefault();
-  pageNum = 1;
+  let newPageNumber = 1;
   refs.galleryList.innerHTML = '';
-  await getPicturesFromApi(refs.searchForm.elements.searchQuery.value, pageNum);
+  await getPicturesFromApi(
+    refs.searchForm.elements.searchQuery.value,
+    newPageNumber
+  );
 }
 export async function onLoadMore() {
   let newPageNumber = pageNum + 1;
